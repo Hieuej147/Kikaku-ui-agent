f4a95f5febe6d7ba87fd4514e46858c2fd484753
Hieuej147 <hihigani@gmail.com>
2025-12-16 17:40:46 +0700

    08-last

---
+++ src/modules/messages/server/procedures.ts
@@ -0,0 +1,40 @@
+import { inngest } from "@/inngest/client";
+import { prisma } from "@/lib/prisma";
+import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+import z from "zod";
+
+export const messageTRPCRouter = createTRPCRouter({
+  getMany: baseProcedure.query(async () => {
+    const messages = await prisma.message.findMany({
+      orderBy: {
+        updatedAt: "desc",
+      },
+      include: {
+        fragment: true,
+      },
+    });
+    return messages;
+  }),
+  create: baseProcedure
+    .input(
+      z.object({
+        value: z.string().min(1, { message: "Message is required" }),
+      })
+    )
+    .mutation(async ({ input }) => {
+      const newMessage = await prisma.message.create({
+        data: {
+          content: input.value,
+          role: "USER",
+          type: "RESULT",
+        },
+      });
+      await inngest.send({
+        name: "code-agent-kikaku/run",
+        data: {
+          value: input.value,
+        },
+      });
+      return newMessage;
+    }),
+});
