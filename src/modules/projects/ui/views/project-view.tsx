e3aa94c63db1e8c130a91682de748e0acc6d20b8
Hieuej147 <hihigani@gmail.com>
2025-12-20 12:44:12 +0700

    update project header

--- src/modules/projects/ui/views/project-view.tsx
+++ src/modules/projects/ui/views/project-view.tsx
@@ -8,19 +8,23 @@ import {
   ResizablePanelGroup,
 } from "@/components/ui/resizable";
 import { MessagesContainer } from "../components/message-container";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
+import { Fragment } from "@/generated/prisma/client";
+import { ProjectHeader } from "../components/project-header";
 
 interface Props {
   projectId: string;
 }
 
 export const Projectview = ({ projectId }: Props) => {
+  const [activeFragment, setActiveFragment] = useState<Fragment | null>(null);
   const trpc = useTRPC();
   // const { data: project } = useSuspenseQuery(
   //   trpc.projects.getOne.queryOptions({
   //     id: projectId,
   //   })
   // );
+  
 
   return (
     <>
@@ -32,12 +36,24 @@ export const Projectview = ({ projectId }: Props) => {
             className="flex flex-col min-h-0"
           >
             {/* {JSON.stringify(project)} */}
+            <Suspense fallback={<p>Loading project...</p>}>
+              <ProjectHeader projectId={projectId} />
+            </Suspense>
             <Suspense fallback={<p>Loading messages...</p>}>
-              <MessagesContainer projectId={projectId} />
+              <MessagesContainer
+                projectId={projectId}
+                activeFragment={activeFragment}
+                setActiveFragment={setActiveFragment}
+              />
             </Suspense>
           </ResizablePanel>
           <ResizableHandle withHandle />
-          <ResizablePanel defaultSize={65} minSize={30} maxSize={80} className="min-h-0">
+          <ResizablePanel
+            defaultSize={65}
+            minSize={30}
+            maxSize={80}
+            className="min-h-0"
+          >
             {/* {JSON.stringify(messages, null, 2)} */}
             TODO: Preview
           </ResizablePanel>
